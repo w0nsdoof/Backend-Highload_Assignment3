24164a026588388c26678c7f632a5aed5d0f6d8c
w0nsdoof <w0nsdoof@gmail.com>
2024-11-03 18:53:38 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-OGGING = {
+LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
